ec9b5f86132de058678d7405acb61a143d9bb658
Heiko Adams <heiko.adams@googlemail.com>
2011-11-20 15:47:41 +0100

    Changed license to Apache 2.0

--- pascal/data.pas
+++ pascal/data.pas
@@ -1,18 +1,17 @@
 {
-This file is part of Bewerbungsmanager.
+  Copyright 2011 Heiko Adams
 
-Bewerbungsmanager is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-any later version.
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-Bewerbungsmanager is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+    http://www.apache.org/licenses/LICENSE-2.0
 
-You should have received a copy of the GNU General Public License
-along with Bewerbungsmanager.  If not, see <http://www.gnu.org/licenses/>.
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 }
 
 unit Data;
@@ -93,8 +92,10 @@ begin
   sSearchPath := ExtractFilePath(Application.ExeName);
   sSearchPath := IncludeTrailingPathDelimiter(sSearchPath) + 'SQL' + PathDelim;
 
+  // Automatisch freien Speicher wieder freigeben
   conData.ExecuteDirect('PRAGMA auto_vacuum = 1;');
 
+  // Sicherstellen, das SQLs in der richtigen Reihenfolge ausgeführt werden ..
   if FindFirst(sSearchPath + '*.sql', faAnyFile, scripts) = 0 then
   begin
     repeat
@@ -104,7 +105,9 @@ begin
 
   FindClose(scripts);
   Files.Sort;
+  // ..
 
+  // Datenbankstruktur erstellen
   Script := TStringList.Create;
 
   for nCount := 0 to Files.Count -1 do
@@ -116,7 +119,6 @@ begin
   end;
 
   Script.Free;
-
   Files.Free;
 end;
 
@@ -237,6 +239,7 @@ begin
   if FNewDB then
     NewDatabase;
 
+  // Referenzielle Integritätsprüfung aktivieren
   TSQLite3Connection(Sender).ExecuteDirect('PRAGMA foreign_keys = ON;');
 
   OpenDataSources;
@@ -244,6 +247,7 @@ end;
 
 procedure TdmBewerbungen.conDataBeforeConnect(Sender: TObject);
 begin
+  // Prüfen, ob eine neue Datenbank erstellt werden muss
   FNewDB:=not FileExists(TSQLite3Connection(Sender).DatabaseName);
 end;
 
