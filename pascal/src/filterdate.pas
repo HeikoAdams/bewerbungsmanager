4ca353b69a061d0f1264dc8cd904cc07e0fa695b
Heiko Adams <heiko.adams@gmail.com>
2015-07-23 09:41:23 +0200

    Relicensed project from apache2 to gpl3

--- pascal/src/filterdate.pas
+++ pascal/src/filterdate.pas
@@ -1,18 +1,21 @@
-{
-This file is part of Bewerbungsmanager.
+{ Bewerbungsmanager - a small tool for managing applications
 
-Bewerbungsmanager is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-any later version.
+  Copyright (C) 2011 Heiko Adams heiko.adams@gmail.com
 
-Bewerbungsmanager is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  This source is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2 of the License, or (at your option)
+  any later version.
 
-You should have received a copy of the GNU General Public License
-along with Bewerbungsmanager.  If not, see <http://www.gnu.org/licenses/>.
+  This code is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  A copy of the GNU General Public License is available on the World Wide Web
+  at <http://www.gnu.org/copyleft/gpl.html>. You can also obtain it by writing
+  to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+  MA 02111-1307, USA.
 }
 
 unit filterdate;
--- pascal/src/newuser.pas
+++ pascal/src/newuser.pas
@@ -1,3 +1,23 @@
+{ Bewerbungsmanager - a small tool for managing applications
+
+  Copyright (C) 2011 Heiko Adams heiko.adams@gmail.com
+
+  This source is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License as published by the Free
+  Software Foundation; either version 2 of the License, or (at your option)
+  any later version.
+
+  This code is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+  details.
+
+  A copy of the GNU General Public License is available on the World Wide Web
+  at <http://www.gnu.org/copyleft/gpl.html>. You can also obtain it by writing
+  to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+  MA 02111-1307, USA.
+}
+
 unit newuser;
 
 {$mode objfpc}{$H+}
@@ -71,9 +91,12 @@ begin
       Free;
     end;
 
-    dmBewerbungen.traData.Commit;
-    dmBewerbungen.qryBenutzer.Close;
-    dmBewerbungen.qryBenutzer.Open;
+    with dmBewerbungen do
+    begin
+      traData.Commit;
+      qryBenutzer.Close;
+      qryBenutzer.Open;
+    end;
     ModalResult:=mrOK;
   end;
 end;
